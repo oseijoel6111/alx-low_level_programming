12abb34286334ea4e7836dc078e49cbdd98e6564
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-14 18:51:42 -0700

    Print Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ * It display numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+		continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
