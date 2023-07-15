bef51ac147017190a8422fee52826c55eb7e498d
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-14 18:36:32 -0700

    Print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ * It display alphabet in lower in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
