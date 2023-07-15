b1130e62b882ace05c02516a521147b3e08f868c
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-14 19:50:13 -0700

    Print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ * It display numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int fTens, fOnes, sTens, sOnes;
+
+	for (fTens = 0; fTens <= 9; fTens++)
+	{
+		for (fOnes = 0; fOnes <= 9; fOnes++)
+		{
+			for (sTens = fTens; sTens <= 9; sTens++)
+			{
+				int startOnes = (sTens == fTens) ? fOnes + 1 : 0;
+
+				for (sOnes = startOnes; sOnes <= 9; sOnes++)
+				{
+					putchar(fTens + '0');
+					putchar(fOnes + '0');
+					putchar(' ');
+					putchar(sTens + '0');
+					putchar(sOnes + '0');
+
+					if (!(fTens == 9 && fOnes == 9 && sTens == 9 && sOnes == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
