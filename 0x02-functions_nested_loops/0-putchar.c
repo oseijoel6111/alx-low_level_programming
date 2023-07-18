bb96b6cd4959d1ba2a2e45f8a3e603a3c2e11cd3
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-18 09:47:06 -0700

    Display putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ *              It will display _putchar,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{   
+        printf("_putchar \n");
+        return (0);
+}
