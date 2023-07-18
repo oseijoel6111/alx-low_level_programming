8bf4ad77e578aaccec3731d980ea0c180ca4a0e1
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-18 14:30:39 -0700

    Display alphabetics 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ *              It will display alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
+
