c838ff26ccf6e67d3f64e69bd5c6914040dec7d2
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-18 16:17:23 -0700

    Print

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ *              It will bsolute value of an integer.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+print_to_98(0);
+print_to_98(98);
+print_to_98(111);
+print_to_98(81);
+print_to_98(-10);
+
+return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -26,5 +26,6 @@ void print_times_table(int n);
 
 int add(int, int);
 
+void print_to_98(int n);
 
 #endif
