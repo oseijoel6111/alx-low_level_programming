46edc059198ffc156b9897dfa3711ca943287351
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-18 14:51:42 -0700

    Is alphabet

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ *              It will display the results of _islower function calls.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
+
