ed30629a6084040e6f998a7461f292fd9f3cfc99
oseijoel6111 <oseijoel6111@gmail.com>
2023-07-18 15:28:59 -0700

    8-24 Hours

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ *              It will bsolute value of an integer.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+jack_bauer();
+return (0);
+}
+
