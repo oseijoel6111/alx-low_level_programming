11a4f0bc041f5180767a3496b25446e83e9c4433
oseijoel6111 <oseijoel6111@gmail.com>
2023-08-01 14:08:32 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+/**
+ * set_string - set the values of a pointers to a char
+ *
+ * @s: pointer to pointers to a char
+ *
+ * @to: the address to match
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
