682a7cb91c581afe29f1413f6326b4c1139fe712
oseijoel6111 <oseijoel6111@gmail.com>
2023-08-14 14:39:51 -0700

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Joel
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       to compute the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif 
+
