747760647a19e16e527d271810b5c83b08c19251
oseijoel6111 <oseijoel6111@gmail.com>
2023-10-16 13:14:25 -0700

    Task 0 added

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn -  gets the length of a substring
+ *
+ * @s: char pointers
+ * @accept: char pointers
+ *
+ *
+ * Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int count1, count2;
+
+	for (count1 = 0; s[count1] != '\0'; count1++)
+	{
+		for (count2 = 0; accept[count2] != '\0'; count2++)
+		{
+			if (s[count1] == accept[count2])
+			{
+				break;
+			}
+		}
+			if (!(accept[count2]))
+			{
+				break;
+			}
+	}
+	return (count1);
+}
+
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  search a string for any of a sets of bytes.
+ *
+ * @s: char pointer
+ *
+ * @accept: char pointer
+ *
+ * Return: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int counter;
+
+	for (; *s != '\0'; s++)
+	{
+		for (counter = 0; accept[counter] != '\0'; counter++)
+		{
+
+			if (*s == accept[counter])
+				return (s);
+		}
+	}
+	return (0);
+}
+
